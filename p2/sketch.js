58d2e26093ebe8aa865fc51b89e85049317610c1
howshekilledit <morganhg@uic.edu>
2022-10-19 15:24:46 -0500

    add number param

--- p2/sketch.js
+++ p2/sketch.js
@@ -1,20 +1,24 @@
+let num;
+
 function setup() {
   let canvas = createCanvas(0.5*windowWidth, 0.5*windowWidth); //create canvas half screen width
   canvas.addClass('centered'); //add centered class to canvas (see style.css)
    //get color value from URL
   let params = getParams();
   let clr = params.color; 
+  num = params.num; 
   //add color values in link to next page
   var a = document.getElementsByTagName('a')[0];
   var href = setParams(a.href, 'color', clr);
   a.href = href;
 
   background(clr);
+  fill('yellow');
   noStroke();
 }
 
 function draw() {
-
-  fill('yellow');
-  ellipse(width/2, height/2, 20);
+  for(var i = 0; i < num; i++){
+    ellipse(width/2, 50+i*30, 20);
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
 
 let clr = 'red';
-let input;
+let clr_input;
+let num_input; 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //add input to set color
-  input = createInput();
-  input.position(0, 150);
-  input.attribute('placeholder', 'type a color');
+  //add clr_input to set color
+  clr_input = createInput();
+  clr_input.position(0, 150);
+  clr_input.attribute('placeholder', 'type a color');
 
-  //add sumit button
+  //add num_input to set number
+  num_input = createInput();
+  num_input.position(0, 100);
+  num_input.attribute('placeholder', 'type a number');
+
+  //add submit button
   button = createButton('submit');
-  button.position(input.x + input.width, input.y);
+  button.position(clr_input.x + clr_input.width, clr_input.y);
 }
 
 function draw() {
@@ -19,8 +25,10 @@ function draw() {
   ellipse(200, 200, 20);
   //on button press
   button.mousePressed(function(){
-    clr = input.value(); //get value from input field
+    clr = clr_input.value(); //get value from clr_input field
+    num = num_input.value(); 
     var href = setParams('./p2', 'color', clr); //set URL navigation to include clr value
+    href = setParams(href, 'num', num); //set URL navigation to include clr value
     window.location.href = href; //navigate to URL set above
   });
 

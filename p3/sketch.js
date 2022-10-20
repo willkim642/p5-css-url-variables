6c1255bcad864102a6e8d3cc4eec759ef7566a0f
willkim642 <111807821+willkim642@users.noreply.github.com>
2022-10-20 12:13:17 -0500

    added image that uses mouseX and mouseY to move

--- p3/sketch.js
+++ p3/sketch.js
@@ -1,15 +1,22 @@
 let clr;
+let img;
+
+function preload() {
+    img = loadImage("image3.jpg");
+}
 
 function setup() {
-  let canvas = createCanvas(200, 200); //create canvas half screen width
+  let canvas = createCanvas(800, 800); //create canvas half screen width
   canvas.addClass('positioned'); //add positioned class to canvas (see style.css)
   let params = getParams(); //get values from URL
   clr = params.color; 
   background('yellow');
-  noStroke();
 }
 
 function draw() {
-  fill(clr);
-  ellipse(random(200), random(200), 20);
+    image(img, mouseX, mouseY);
+    img.resize(100, 100);
+    //fill(clr);
+    
+  //ellipse(random(800), random(800), 20);
 }
